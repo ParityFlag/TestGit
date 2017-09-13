cccd4c42d2679fba5ee648cc001d5e72d8704984
ParityFlag <982387134@qq.com>
2017-09-13 19:59:28 +0800

    20170913 19:59 lxn change u1

--- src/Test2.java
+++ src/Test2.java
@@ -1,3 +1,20 @@
 public class Test2 {
     private String lxn;
+    private String zf;
+
+    public String getLxn() {
+        return lxn;
+    }
+
+    public void setLxn(String lxn) {
+        this.lxn = lxn;
+    }
+
+    public String getZf() {
+        return zf;
+    }
+
+    public void setZf(String zf) {
+        this.zf = zf;
+    }
 }
